fd21a91d7fd7e7139a6f60278cb0cd93db74a281
Arlan Tri Handika <handikaarlan@gmail.com>
2024-06-20 23:34:10 +0700

    add styling at home page

--- index.php
+++ index.php
@@ -3,33 +3,39 @@ $title = "Cerita SI-3 - Beranda";
 ob_start();
 
 session_start();
-
-
 if (!isset($_SESSION['user'])) {
     header("Location: login.php");
     exit();
 }
 
 include "config/connection.php";
+
+$sql = "SELECT p.*, ph.photo_file
+                FROM tb_post p
+                LEFT JOIN tb_photos ph ON p.post_id = ph.photo_id_post";
+
+$result = $conn->query($sql);
 ?>
 
 <h2>Kegiatan Kelas SI-3</h2>
-<?php
-$sql = "SELECT * FROM tb_post";
-$result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        echo "<div class='post'>";
-        echo "<h3><a href='detail_post.php?post_id=" . $row['post_id'] . "'>" . $row['post_title'] . "</a></h3>";
-        echo "<p>" . substr($row['post_text'], 0, 100) . "...</p>";
-        echo "<p><strong>Date: </strong>" . $row['post_date'] . "</p>";
-        echo "</div>";
+<div class="posts-container">
+    <?php
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            echo "<div class='post'>";
+            echo "<a href='detail_post.php?post_id=" . $row['post_id'] . "'><h3>" . $row['post_title'] . "</h3></a>";
+            echo "<a href='detail_post.php?post_id=" . $row['post_id'] . "'><img src='assets/images/" . $row['photo_file'] . "'></a>";
+            echo "<p>" . substr($row['post_text'], 0, 100) . "...</p>";
+            echo "<span>Tanggal Upload: " . $row['post_date'] . "</span>";
+            echo "</div>";
+        }
+    } else {
+        echo "<p>Tidak ada kegiatan yang ditemukan.</p>";
     }
-} else {
-    echo "No activities found.";
-}
-
+    ?>
+</div>
+<?php
 $content = ob_get_clean();
 include "layouts/app.php";
 ?>
